89c75583fec31a0852a1b231e66351181fdab945
wikla pandu <wiklapandu2503@gmail.com>
2022-10-10 23:02:46 +0700

    create token check middleware

--- app.js
+++ app.js
@@ -18,7 +18,51 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-router.get("/", (req, res) => {
+/* helper function */
+function fetchUserByToken(req) {
+  return new Promise((resolve, reject) => {
+    if (req.headers && req.headers.authorization) {
+      let auth = req.headers.authorization;
+      let decoded;
+
+      try {
+        decoded = jwt.verify(auth, process.env.SECRET_JWT_CODE);
+      } catch (e) {
+        reject("Invalid Token");
+        return;
+      }
+
+      let userId = decoded.id;
+      User.findOne({ _id: userId })
+        .then((user) => {
+          resolve(user);
+        })
+        .catch((err) => {
+          reject("Token error");
+        });
+    } else {
+      reject("No Token Found");
+    }
+  });
+}
+
+/* middleware function */
+
+function checkToken(req, res, next) {
+  fetchUserByToken(req)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      res.json({
+        status: "fail",
+        error: err,
+      });
+    });
+}
+
+router.get("/", checkToken, (req, res) => {
   return res.json("test");
 });
 
